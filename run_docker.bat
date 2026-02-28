bdb316eb4e99c05180959d9f63902932093e76e7
chicvill <himin5004@gmail.com>
2026-02-28 09:11:36 +0900

    Configure multi-farm Docker setup and share Seoul/Busan data configs

--- run_docker.bat
+++ run_docker.bat
@@ -4,6 +4,10 @@ docker-compose up --build -d
 
 echo.
 echo [!] 컨테이너가 실행되었습니다.
-echo [!] 웹 브라우저에서 'http://localhost:8000'으로 접속하세요.
+echo [!] 각 농장 시스템에 다음 주소로 접속하세요:
+echo [!] 1. 서울 농장: http://localhost:8001
+echo [!] 2. 부산 농장: http://localhost:8002
+echo [!] 3. 기본 시스템: http://localhost:8000
+echo.
 echo [!] 로그를 보려면 'docker-compose logs -f'를 입력하세요.
 pause
